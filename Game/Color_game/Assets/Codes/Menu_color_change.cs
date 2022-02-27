e6d112a430135e85ac1b06db66a4ab68ee7adbd6
Gabor <gabor.faust25@gmail.com>
2022-02-27 09:40:54 +0100

    Build plus code change

--- Game/Color_game/Assets/Codes/Menu_color_change.cs
+++ Game/Color_game/Assets/Codes/Menu_color_change.cs
@@ -10,20 +10,33 @@ public class Menu_color_change : MonoBehaviour
     public List<GameObject> texts;
     public float random1, random2, random3;
     private System.Random r = new System.Random();
+    private float timer;
+
+    private void Start()
+    {
+        timer = 0;
+    }
 
     void Update()
     {
-        random1 += (float)(r.Next(1,100) / 1000);
-        random2 += (float)(r.Next(1,100) / 1000);
-        random3 += (float)(r.Next(1,100) / 1000);
+        timer += Time.deltaTime;
+
+        random1 += (float)(r.Next(1, 100) / 1000f);
+        random2 -= (float)(r.Next(1, 100) / 1000f);
+        random3 += (float)(r.Next(1, 100) / 1000f);
 
         if (random1 >= 255) random1 = 0;
-        if (random2 >= 255) random2 = 0;
+        if (random2 <= 0) random2 = 255;
         if (random3 >= 255) random3 = 0;
 
-        foreach (var item in texts)
+        if (timer > 1)
         {
-            item.GetComponent<TextMeshProUGUI>().color = new Color(random1, random2, random3);
+            timer = 0;
+
+            foreach (var item in texts)
+            {
+                item.GetComponent<TextMeshProUGUI>().color = new Color(random1, random2, random3);
+            }
         }
     }
 }
