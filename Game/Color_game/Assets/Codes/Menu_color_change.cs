f45ec540faf115551a7200d0c021a33abdec7ff2
Gabor <gabor.faust25@gmail.com>
2022-03-05 11:44:26 +0100

    Make Code Pretty

--- Game/Color_game/Assets/Codes/Menu_color_change.cs
+++ Game/Color_game/Assets/Codes/Menu_color_change.cs
@@ -19,22 +19,29 @@ public class Menu_color_change : MonoBehaviour
     {
         timer = 0;
 
-        random1 = r.Next(1, 255) / 255f;
-        random2 = r.Next(1, 255) / 255f;
-        random3 = r.Next(1, 255) / 255f;
-
-        sign1 = 1;
-        sign2 = 1;
-        sign3 = 1;
-
         color_blocker = r.Next(1, 7);
+
+        switch (color_blocker)
+        {
+            case 1: random1 = r.Next(1, 255); break;
+            case 2: random2 = r.Next(1, 255); break;
+            case 3: random3 = r.Next(1, 255); break;
+            case 4: { random1 = r.Next(1, 255); random2 = r.Next(1, 255); break; }
+            case 5: { random1 = r.Next(1, 255); random3 = r.Next(1, 255); break; }
+            case 6: { random2 = r.Next(1, 255); random3 = r.Next(1, 255); break; }
+            default: break;
+        }
     }
 
     void Update()
     {
         Select_Random_Color();
+        //Select_Random_Color2(); another solution, but it does not look that great
+        //Select_Random_Color3(); random color, causes eye bleed
+    }
 
-        /*
+    private void Select_Random_Color3()
+    {
         timer += Time.deltaTime;
 
         if (timer > 0.5f)
@@ -48,7 +55,6 @@ public class Menu_color_change : MonoBehaviour
                 item.GetComponent<TextMeshProUGUI>().color = new UnityEngine.Color(random1 / 255f, random2 / 255f, random3 / 255f, 255f);
             }
         }
-        */
     }
 
     private void Select_Random_Color()
@@ -75,6 +81,27 @@ public class Menu_color_change : MonoBehaviour
             case 6: { random2 = 0; random3 = 0; break; }
             default: break;
         }
+
+        foreach (var item in texts)
+        {
+            item.GetComponent<TextMeshProUGUI>().color = new UnityEngine.Color(random1 / 255f, random2 / 255f, random3 / 255f, 255f);
+        }
+    }
+
+    private void Select_Random_Color2()
+    {
+        if (color_blocker == 2 || color_blocker == 3 || color_blocker == 6) random1 += sign1 * (float)(r.Next(1, 100) / 1000f);
+        if (color_blocker == 1 || color_blocker == 3 || color_blocker == 5) random2 += sign2 * (float)(r.Next(1, 100) / 1000f);
+        if (color_blocker == 1 || color_blocker == 2 || color_blocker == 4) random3 += sign3 * (float)(r.Next(1, 100) / 1000f);
+
+        if (random1 >= 255) sign1 = -1;
+        if (random2 >= 255) sign2 = -1;
+        if (random3 >= 255) sign3 = -1;
+
+        if (random1 <= 0) sign1 = 1;
+        if (random2 <= 0) sign2 = 1;
+        if (random3 <= 0) sign3 = 1;
+
 
         foreach (var item in texts)
         {
