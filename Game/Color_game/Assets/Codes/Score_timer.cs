7018af28032bd87836e4c4de0adb660a97f8ed79
Gabor <gabor.faust25@gmail.com>
2022-04-05 17:55:32 +0200

    Timer added to games (for score)

---
+++ Game/Color_game/Assets/Codes/Score_timer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class Score_timer : MonoBehaviour
+{
+    private float timer = 0;
+    [SerializeField] private GameObject text;
+
+    void Start()
+    {
+        Debug.Log(PlayerPrefs.GetFloat("GrayScale")+ "gray");
+        Debug.Log(PlayerPrefs.GetFloat("Diamond") + "diamon");
+    }
+
+    void Update()
+    {
+        if(timer < (float.MaxValue-10))
+        {
+            timer += Time.deltaTime;
+            text.GetComponent<TextMeshProUGUI>().text = timer.ToString("00.00")+" sec";
+        }
+    }
+
+    public void Save_Time()
+    {
+        if (SceneManager.GetActiveScene().name.Contains("Gray"))
+        {
+            PlayerPrefs.SetFloat("GrayScale", timer);
+        }
+        if (SceneManager.GetActiveScene().name.Contains("Diamond"))
+        {
+            PlayerPrefs.SetFloat("Diamond", timer);
+        }
+        PlayerPrefs.Save();
+    }
+}
