47866f1928159124c2be421f5cf1ce55e371a239
Gabor <gabor.faust25@gmail.com>
2022-04-25 19:40:47 +0200

    Fix lights

---
+++ Game/Color_game/Assets/Codes/Spawn_cubes_for_grayscale.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn_cubes_for_grayscale : MonoBehaviour
+{
+    public GameObject game_handler;
+
+    private bool left;
+
+    void Start()
+    {
+        left = false;
+        if (this.gameObject.name.Contains("left")) left = true;
+
+        Spawn();
+    }
+
+    private void Spawn()
+    {
+        GameObject spawn_obj = game_handler.GetComponent<Game_handler>().Next_cube(left);
+        if(spawn_obj != null)
+        Instantiate(spawn_obj, this.transform.position+Vector3.up, this.transform.rotation);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag != "Background")
+        {
+            Spawn();
+        }
+    }
+}
